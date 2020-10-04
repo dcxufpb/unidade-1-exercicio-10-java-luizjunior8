9a2940a548d215a7762ecd3ba9013c04cd746e7c
luizjunior8 <luiz.oliveira@dcx.ufpb.br>
2020-10-04 03:33:03 -0300

    Mission Accomplished

--- src/main/java/com/example/project/Loja.java
+++ src/main/java/com/example/project/Loja.java
@@ -79,9 +79,87 @@ public class Loja {
         return this.inscricaoEstadual;
     }
 
+    public static String Spacer = System.lineSeparator();
+
+    public static boolean checker (String tested){
+		String k = "";
+		k += tested;
+		if (k.equals("null")|| k.isEmpty()){
+			return false;
+		}	
+		return true;	
+    }
+    
+    public void valida_dados_obrigatorios() {
+
+        if (checker(this.getNomeLoja()) == false){
+			throw new RuntimeException ("O campo nome da loja é obrigatório");
+		}
+
+		if (checker(this.getLogradouro()) == false){
+			throw new RuntimeException ("O campo logradouro do endereço é obrigatório");
+        }
+        
+        if (checker(this.getMunicipio()) == false){
+			throw new RuntimeException ("O campo município do endereço é obrigatório");			
+        }
+
+        if (checker(this.getEstado()) == false){
+			throw new RuntimeException ("O campo estado do endereço é obrigatório");
+        }
+        
+        if (checker(this.getCnpj()) == false){
+			throw new RuntimeException ("O campo cnpj da loja é obrigatório");
+        }
+        
+        if (checker(this.getInscricaoEstadual()) == false){
+			throw new RuntimeException ("O campo inscrição estadual da loja é obrigatório");
+		}
+    }
+
     public String dadosLoja() {
-		// Implemente aqui
-		return null;
+
+		String _logradouro = this.getLogradouro()+ ", ";
+		
+		String _numero = "0";
+
+		if (this.getNumero() == 0){
+			_numero = "s/n";
+		}else{
+			_numero = String.format ("%d", this.getNumero());
+		}
+
+		String _complemento = (checker(this.getComplemento()) == false) ? "" : " " + this.getComplemento();
+
+        String _bairro = checker(this.getBairro()) == false ? "" : this.getBairro() + " - ";
+        
+        String _municipio = this.getMunicipio() + " - ";
+
+        String _cep = "";
+
+		if (checker(this.getCep()) == true){
+			_cep = "CEP:" + this.getCep();
+		}
+
+		String _telefone = checker(this.getTelefone()) == false? "" : "Tel " + this.getTelefone();
+
+	 	if (!_cep.isEmpty() && !_telefone.isEmpty()){
+			_telefone = " Tel " + this.getTelefone();
+		}
+		
+        String _observacao = checker(this.getObservacao()) == false ? "" : this.getObservacao();
+        
+        String _cnpj = "CNPJ: " + this.getCnpj();
+
+        String _inscricao_estadual = "IE: " + this.getInscricaoEstadual(); 
+
+		return (this.getNomeLoja() + Spacer +
+		 _logradouro + _numero + _complemento + Spacer +
+		 _bairro + _municipio + this.getEstado() + Spacer +
+		 _cep + _telefone + Spacer +
+		 _observacao + Spacer +	
+		 _cnpj + Spacer +
+		 _inscricao_estadual + Spacer);
 	}
 
 }
--- src/test/java/com/example/project/TestLoja.java
+++ src/test/java/com/example/project/TestLoja.java
@@ -194,7 +194,7 @@ public class TestLoja {
 		verificarCampoObrigatorio("O campo inscrição estadual da loja é obrigatório", ieVazia);
 
 		Loja ieNula = new Loja(NOME_LOJA, LOGRADOURO, NUMERO, COMPLEMENTO, BAIRRO, MUNICIPIO, ESTADO, CEP, TELEFONE,
-				OBSERVACAO, CNPJ, "");
+				OBSERVACAO, CNPJ, null);
 		verificarCampoObrigatorio("O campo inscrição estadual da loja é obrigatório", ieNula);
 	}
 
@@ -215,24 +215,30 @@ public class TestLoja {
 	@Test
 	public void exercicio02_Customizado() {
 		// Defina seus próprios valores para as variáveis a seguir
-		String nomeLoja = "";
-		String logradouro = "";
-		int numero = 0;
-		String complemento = "";
-		String bairro = "";
-		String municipio = "";
-		String estado = "";
-		String cep = "";
-		String telefone = "";
-		String observacao = "";
-		String cnpj = "";
-		String inscricaoEstadual = "";
+		String nomeLoja = "Jr Tech";
+		String logradouro = "Rua Geraldo Correia de Melo";
+		int numero = 100;
+		String complemento = "Casa";
+		String bairro = "Centro";
+		String municipio = "Araçagi";
+		String estado = "PB";
+		String cep = "58270-000";
+		String telefone = "(83) 98111-2697";
+		String observacao = "Matriz";
+		String cnpj = "66.651.293/0001-85";
+		String inscricaoEstadual = "222.333.444.555";
 
 		Loja lojaCustomizada = new Loja(nomeLoja, logradouro, numero, complemento, bairro, municipio, estado, cep,
 				telefone, observacao, cnpj, inscricaoEstadual);
 
 		// E atualize o texto esperado abaixo
-		rodarTestarRetorno("" + BREAK, lojaCustomizada);
+		rodarTestarRetorno("Jr Tech" + BREAK + 
+		"Rua Geraldo Correia de Melo, 100 Casa" + BREAK + 
+		"Centro - Araçagi - PB" + BREAK + 
+		"CEP:58270-000 Tel (83) 98111-2697" + BREAK + 
+		"Matriz" + BREAK + 
+		"CNPJ: 66.651.293/0001-85" + BREAK + 
+		"IE: 222.333.444.555" + BREAK, lojaCustomizada);
 	}
 
 	private void rodarTestarRetorno(String expected, Loja loja) {
